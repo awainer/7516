97d76ef8e2e9cd73285981383d38344b82962350
Ariel Wainer <ari@ariwainer.com.ar>
2015-08-23 14:57:47 -0300

    Separo el scanner en una clase aparte.

--- scanner.py
+++ scanner.py
@@ -1,78 +1,70 @@
 from ply import lex
 import sys
 
-# Lista de tokens
-tokens = ["number", 
-          "equal",
-          "not_equal",
-          "comma",
-          "semicolon",
-          "assign",
-          "logic_operation",
-          "add",
-          "substract",
-          "multiply",
-          "divide",
-          "open_parenthesis",
-          "close_parenthesis",
-          "ident",
-          "string",
-          "program_end",
-          "newline",
-          ]
-# http://stackoverflow.com/questions/5022129/ply-lex-parsing-problem
-reserved = [ 'const', 'var', 'procedure', 'call', 'if', 'then', 'while', 'do', 'begin', 'end', 'odd', 'write', 'writeln', 'readln' ]
-tokens += reserved
 
-# Definicion de tokens
-def t_number(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-t_const = 'const '
-t_equal = '='
-t_not_equal = '<>'
-t_comma = ','
-t_semicolon = ';'
-t_var = "VAR"
-t_procedure = "procedure"
-t_assign = ":="
-t_call = "CALL"
-t_begin = "BEGIN"
-t_end = "END"
-t_if = "IF"
-t_then = "THEN"
-t_while = "WHILE"
-t_do = "DO"
-t_odd = "odd"
-t_logic_operation = r"(=|<=|>=|<|>)"
-t_add = "\+"
-t_substract = "-"
-t_multiply = "\*"
-t_divide = "/"
-t_open_parenthesis = "\("
-t_close_parenthesis = "\)"
-def t_ident(t):
-    r"[a-z|A-Z]\w{0,10}"
-    if t.value.lower() in reserved:
-        t.type = t.value.lower()
-    return t
-t_ignore = r" \t"
-t_writeln = "writeln" 
-t_write = "write"
-t_string = "'.*'"
-t_program_end = "\."
-def t_newline(t):
-    r'[\n\r]+'
-    t.lexer.lineno += t.value.count('\n')
+class Scanner(object):
 
-def t_error(t):
-    raise TypeError("Unknown text '%s' at %s" % (t.value,t.lexer.lineno))
-
-lex.lex(debug=1)
-lex.input(open(sys.argv[1],'r').read())
+    # Lista de tokens
+    tokens = ["number", "equal", "not_equal", "comma", "semicolon", "assign", "logic_operation", "add", "substract", "multiply", "divide", "open_parenthesis", "close_parenthesis", "ident", "string", "program_end", "newline", ]
+    # http://stackoverflow.com/questions/5022129/ply-lex-parsing-problem
+    reserved = [ 'const', 'var', 'procedure', 'call', 'if', 'then', 'while', 'do', 'begin', 'end', 'odd', 'write', 'writeln', 'readln' ]
+    tokens += reserved
+    
+    def __init__(self,filename):
+        lex.lex(module=self)
+        with open(filename,'r') as f:
+            lex.input(f.read())        
+    
+    def next_token(self):
+        return lex.token()
+    
+    # Definicion de tokens
+    def t_number(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
+    t_const = 'const '
+    t_equal = '='
+    t_not_equal = '<>'
+    t_comma = ','
+    t_semicolon = ';'
+    t_var = "VAR"
+    t_procedure = "procedure"
+    t_assign = ":="
+    t_call = "CALL"
+    t_begin = "BEGIN"
+    t_end = "END"
+    t_if = "IF"
+    t_then = "THEN"
+    t_while = "WHILE"
+    t_do = "DO"
+    t_odd = "odd"
+    t_logic_operation = r"(=|<=|>=|<|>)"
+    t_add = "\+"
+    t_substract = "-"
+    t_multiply = "\*"
+    t_divide = "/"
+    t_open_parenthesis = "\("
+    t_close_parenthesis = "\)"
+    
+    def t_ident(self, t):
+        r"[a-z|A-Z]\w{0,10}"
+        if t.value.lower() in self.reserved:
+            t.type = t.value.lower()
+        return t
+    t_ignore = " \t"
+    t_writeln = "writeln" 
+    t_write = "write"
+    t_string = "'.*'"
+    t_program_end = "\."
+    
+    def t_newline(self, t):
+        r'[\n\r]+'
+        t.lexer.lineno += t.value.count('\n')
+    
+    def t_error(self, t):
+        raise TypeError("Unknown text '%s' at %s" % (t.value, t.lexer.lineno))
+    
 
 
-for tok in iter(lex.token, None):
-    print tok.lineno,':',
-    print repr(tok.type), repr(tok.value)
+    
