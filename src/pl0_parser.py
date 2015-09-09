5905338c48db123a0d8eaa5aeabf13dc8d5f2719
AriW <awainer@gmail.com>
2015-09-08 21:55:17 -0300

    Estructura nuevo parser.

--- src/pl0_parser.py
+++ src/pl0_parser.py
@@ -3,8 +3,7 @@ Created on 23/8/2015
 
 @author: ari
 '''
-import sys
-from ply import yacc
+
 import logging
 
 
@@ -14,67 +13,36 @@ class Parser():
         self.tokens = scanner.tokens
         self.log = logging.getLogger()
         self.log.setLevel(logging.DEBUG)
-        self.log.addHandler(logging.StreamHandler(stream=sys.stdout))
-        self._parser = yacc.yacc(debug=logging.getLogger(), module=self)
 
     def parse(self, debug=0):
         self.log.info("Empezando parse")
-        return self._parser.parse(lexer=self.scanner.get_lexer(), debug=debug)
+        self.next_token = self.scanner.next_token()
+        while tok:
+            print(tok.type)
+            tok = self.scanner.next_token()
+        return True
+    
+    def parse_program(self):
+        self.parse_block()
+        assert(self.next_token.type=program_end)
+    def parse_block(self):
+        if self.next_token.type == 'const':
+            # declaracion de constantes
+        elif self.next_token.type == 'var':
+            # declaracion de variables
+        else:
+            self.parse_statement()
+        
+    def parse_statement(self):
+        pass
+    def parse_factor(self):
+        pass
+    def parse_term(self):
+        pass
+    def parse_expression(self):
+        pass
+    def parse_condition(self):
+        pass
 
-    start = 'program'
-
-    def p_program(self, p):
-        'program :  block program_end'
-        self.log.info("Parseando program %s" % p[1])
-        p[0] = p[1]
-
-    def p_block(self, p):
-        '''
-          block : const_decl var_decl proc_decl
-        '''
-        self.log.info("Parseando block: %s" % p[1:])
-        p[0] = p[1:]
-
-    def p_const_decl(self, p):
-        '''
-        const_decl : const const_assignment_list
-                    |  const const_assignment_list semicolon
-                    |
-        '''
-        self.log.info("Parseando declaracion de constantes: %s" % p[1:])
-        p[0] = p[1:]
-
-    def p_const_assignment_list(self, p):
-        '''
-        const_assignment_list : ident equal number
-                                | const_assignment_list comma const_assignment_list
-                                | ident
-
-        '''
-        self.log.info("Parseando assigment list: %s" % p[1:])
-        p[0] = p[1:]
-
-    def p_var_decl(self, p):
-        '''
-        var_decl : var ident_list semicolon
-                 |
-        '''
-        self.log.info("Parseando declaracion de variables: %s" % p[1:])
-        p[0] = p[1:]
-
-    def p_ident_list(self, p):
-        '''
-        ident_list : ident
-                   | ident_list comma ident
-        '''
-    def p_proc_decl(self, p):
-        '''
-        proc_decl : procedure ident semicolon block semicolon
-                  |
-        '''
-
-    def p_error(self, p):
-        if not p:
-            return
-        raise ValueError("Syntax error at '%s' line " % p.value, p.lexer.lineno)
+    
 
