3c89e8277e6f6906b241800555bd43d698b28410
Ariel Wainer <ari@ariwainer.com.ar>
2015-09-09 23:15:39 -0300

    Sigo con implementación del parser.

--- src/pl0_parser.py
+++ src/pl0_parser.py
@@ -5,69 +5,180 @@ Created on 23/8/2015
 '''
 
 import logging
-import sys
 
 
 class Parser():
     def __init__(self, scanner):
         self.scanner = scanner
         self.tokens = scanner.tokens
-        self.log = logging.getLogger()
-        self.log.setLevel(logging.DEBUG)
+        self.log = logging.getLogger('parser')
+        if not len(self.log.handlers):
+            self.log.setLevel(logging.DEBUG)
+            hdlr = logging.FileHandler('/tmp/myapp.log')
+            formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+            hdlr.setFormatter(formatter)
+            self.log.addHandler(hdlr)
 
     def read_token(self):
         self.next_token = self.scanner.next_token()
+        self.log.info("Se lee token %s" % self.next_token)
+        print("Se lee token %s" % self.next_token)
 
     def parse(self, debug=0):
+        self.log.info("****************************")
         self.log.info("Empezando parse")
-        self.read_token()
-
+        self.parse_program()
+        self.log.info("Fin parse")
         return True
 
     def error(self, s):
         raise ValueError(s)
 
+    def error_expected(self, expected):
+        raise ValueError("Se espèraba %s se recibio %s" % (expected, self.next_token))
+
     def parse_program(self):
+        self.read_token()
         self.parse_block()
-        if not self.next_token.type == program_end:
-            error("Se esperaba punto")
+        if not self.next_token.type == 'program_end':
+            self.error("Se esperaba punto, se obtuvo: " + str(self.next_token))
 
     def parse_const_decl(self):
+        last_id = ''
         while self.next_token.type != "semicolon":
             self.read_token()
             if self.next_token.type == "ident":
                 last_id = self.next_token.value
                 self.log.info("Declaro constante " + self.next_token.value)
+                last_id = self.next_token.value
             elif self.next_token.type == "comma":
                 pass
-            elif self.next_token.type == "equals":
+            elif self.next_token.type == "equal":
                 self.read_token()
                 if self.next_token.type == "number":
-                    self.log.info("Inicializo  %s con %s" % (last_id,
-                                    self.next_token.value))
+                    self.log.info("Inicializo  constante %s con %s" % (last_id,
+                                                                       self.next_token.value))
                 else:
-                    self.error("Se esperaba un numero, pero se encontro:"
-                    + self.next_token.type)
+                    self.error("Se esperaba un numero, pero se encontro:" +
+                               self.next_token.type)
+            elif self.next_token.type == 'semicolon':
+                self.read_token()
+                return
             else:
-                self.error("Error, se esperaba")
+                self.error("Error, token inesperado: " + str(self.next_token))
+
+    def assert_type(self, expected_type):
+        if not self.next_token.type == type:
+            self.error_expected(expected_type)
+
+    def parse_var_decl(self):
+        last_id = ''
+        while self.next_token.type != "semicolon":
+            self.read_token()
+            if self.next_token.type == "ident":
+                last_id = self.next_token.value
+                self.log.info("Declaro variable " + self.next_token.value)
+                last_id = self.next_token.value
+            elif self.next_token.type == "comma":
+                pass
+            elif self.next_token.type == "equal":
+                self.read_token()
+                if self.next_token.type == "number":
+                    self.log.info("Inicializo  variable %s con %s" % (last_id,
+                                                                      self.next_token.value))
+                else:
+                    self.error("Se esperaba un numero, pero se encontro:" +
+                               self.next_token.type)
+            elif self.next_token.type == 'semicolon':
+                self.read_token()
+                return
+            else:
+                self.error("Error, token inesperado: " + str(self.next_token))
+
+    def parse_procedure_decl(self):
+        self.read_token()
+        self.assert_type('ident')
+        self.log.info("Parseando procedimiento: %s" % self.next_token.value)
+        self.read_token()
+        self.assert_type('semicolon')
+        self.read_token()
+        self.parse_block()
+        self.assert_type('semicolon')
+        self.read_token()
 
     def parse_block(self):
-        last_id = ''
+        self.log.debug('Parseando bloque')
         if self.next_token.type == 'const':
             self.parse_const_decl()
-        elif self.next_token.type == 'var':
-            # declaracion de variables
-        else:
-            self.parse_statement()
+
+        if self.next_token.type == 'var':
+            self.parse_var_decl()
+
+        while self.next_token.type == 'procedure':
+            self.parse_procedure_decl()
+
+        self.parse_statement()
+        self.log.debug('Fin parseando bloque')
 
     def parse_statement(self):
-        pass
+        if self.next_token.type == 'ident':
+            self.read_token()
+            # last_id = self.last_token.value
+            self.assert_type('assign')
+            self.read_token()
+            self.read_token()
+            self.parse_expression()
+
+        elif self.next_token.type == 'call':
+            self.read_token()
+            self.assert_type('ident')
+
+        elif self.next_token.type == 'begin':
+            self.read_token()
+            self.parse_statement()
+            while not self.next_token.type == 'end':
+                self.assert_type('semicolon')
+                self.read_token()
+                self.parse_statement()
+
+        elif self.next_token.type == 'if':
+            self.read_token()
+            self.parse_condition()
+            self.assert_type('then')
+            self.read_token()
+            self.parse_statement()
+        elif self.next_token.type == 'while':
+            self.read_token()
+            self.parse_condition()
+            self.assert_type('do')
+            self.read_token()
+            self.parse_statement()
+
     def parse_factor(self):
-        pass
+        if self.next_token.type == 'ident':
+            self.read_token()
+            return
+        if self.next_token.type == 'number':
+            self.read_token()
+            return
+        if self.next_token.type == 'open_parenthesis':
+            self.read_token()
+            self.parse_expression()
+            self.read_token()
+            self.assert_type('close_parenthesis')
+            self.read_token()
+
     def parse_term(self):
-        pass
+        self.parse_term()
+        while self.next_token.type == 'multiply' or 'divide':
+            # TODO ver que op es
+            self.read_token()
+            self.parse_factor()
+        self.read_token()
+
     def parse_expression(self):
         pass
+
     def parse_condition(self):
         pass
 
--- test/test_parser.py
+++ test/test_parser.py
@@ -4,8 +4,6 @@ Created on 23/8/2015
 @author: ari
 '''
 import unittest
-import sys
-sys.path.append("../src")
 from io import StringIO
 import scanner
 import pl0_parser
@@ -23,23 +21,23 @@ class Test(unittest.TestCase):
         txt = self.get_txt(text)
         my_scanner = scanner.Scanner(txt)
         my_parser = pl0_parser.Parser(my_scanner)
-        self.assert_(my_parser.parse(debug=1))
+        my_parser.parse(debug=1)
         txt.close()
 
     def test_parse_minimum_program(self):
         self.generic_test(".")
 
     def test_parse_single_const(self):
-        self.generic_test("const A.")
+        self.generic_test("const A;.")
 
     def test_parse_single_const_sc(self):
         self.generic_test("const A=6;.")
 
     def test_parse_multiple_const(self):
-        self.generic_test("const A,B.")
+        self.generic_test("const A,B;.")
 
     def test_parse_multiple_const_with_assigment(self):
-        self.generic_test("const A=1,B.")
+        self.generic_test("const A=1,B;.")
 
     def test_single_var_decl(self):
         self.generic_test("var a;.")
@@ -51,10 +49,13 @@ class Test(unittest.TestCase):
         self.generic_test("const a,b,c; var d, e , f;.")
 
     def test_proc_decl_empty_body(self):
-        self.generic_test("procedure garompa; ; .")
+        self.generic_test("procedure foo; ; .")
 
     def test_proc_decl_with_body(self):
-        self.generic_test("procedure garompa;const a;;.")
+        self.generic_test("procedure foo;const a;;.")
+
+    def test_parse_assign(self):
+        self.generic_test("a := 1.")
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
